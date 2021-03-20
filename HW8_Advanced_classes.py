5f187040016ebea1933f0d17bd32a2ddd416a7c9
MarianaDrozd <mariasyad@gmail.com>
2021-03-20 23:27:57 +0200

    Refactoring...

--- HW8_Advanced_classes.py
+++ HW8_Advanced_classes.py
@@ -128,84 +128,94 @@ if __name__ == "__main__":
     print(f"These animals survived in the forest: {list(forest.animals.values())}")
 
 # Output:
-# /usr/bin/python3.8 /home/mariana/Документи/CURSOR/Homeworks/cursor_my_homeworks/HW8_Advanced_classes.py
-# =====Added new animal Herbivorous 96b8486c-e1bc-4de9-8196-ff7401e0db6e, power:79, speed: 40=====
-# =====Added new animal Predator 50038426-1d14-4fde-a0e3-a4528f29f679, power:46, speed: 70=====
-# =====Added new animal Predator 6e072811-2bbf-413e-a658-b458dac4deb9, power:29, speed: 48=====
-# =====Added new animal Herbivorous 1094c043-fc6a-457b-b54d-ce96f63ab824, power:26, speed: 88=====
-# =====Added new animal Predator 1f979e15-5310-4e9c-8e74-8ab98cee009a, power:87, speed: 60=====
-# =====Added new animal Herbivorous c2ec45e3-0da2-48b7-a20c-9942848ccefa, power:75, speed: 58=====
-# =====Added new animal Herbivorous cf52cd00-ed2c-467d-ab72-0ef43f4d3c3c, power:29, speed: 84=====
-# =====Added new animal Herbivorous 073225a8-c7c5-40e7-8be4-65195826cdff, power:56, speed: 69=====
-# =====Added new animal Herbivorous 75ebe620-5598-4cf4-a2c3-124546249918, power:64, speed: 43=====
-# =====Added new animal Predator 4790bb9a-054a-419c-a30a-2e40aef6d59e, power:42, speed: 80=====
-# Herbivorous 96b8486c-e1bc-4de9-8196-ff7401e0db6e is eating. Its power became 79.
-# >>>>>The Predator 50038426-1d14-4fde-a0e3-a4528f29f679 is hunting the Prey 6e072811-2bbf-413e-a658-b458dac4deb9...<<<<<
-# The Predator 50038426-1d14-4fde-a0e3-a4528f29f679 kills the Prey 6e072811-2bbf-413e-a658-b458dac4deb9. The Predator's power is 46
-# =====Removing dead animal 6e072811-2bbf-413e-a658-b458dac4deb9...=====
-# >>>>>The Predator 6e072811-2bbf-413e-a658-b458dac4deb9 is hunting the Prey c2ec45e3-0da2-48b7-a20c-9942848ccefa...<<<<<
-# The Predator's 6e072811-2bbf-413e-a658-b458dac4deb9 power became 20.
-# The Prey's Herbivorous c2ec45e3-0da2-48b7-a20c-9942848ccefa power became 52.
-# Herbivorous 1094c043-fc6a-457b-b54d-ce96f63ab824 is eating. Its power became 26.
-# >>>>>The Predator 1f979e15-5310-4e9c-8e74-8ab98cee009a is hunting the Prey 1094c043-fc6a-457b-b54d-ce96f63ab824...<<<<<
-# The Predator's 1f979e15-5310-4e9c-8e74-8ab98cee009a power became 61.
-# The Prey's Herbivorous 1094c043-fc6a-457b-b54d-ce96f63ab824 power became 18.
-# Herbivorous c2ec45e3-0da2-48b7-a20c-9942848ccefa is eating. Its power became 75.
-# Herbivorous cf52cd00-ed2c-467d-ab72-0ef43f4d3c3c is eating. Its power became 29.
-# Herbivorous 073225a8-c7c5-40e7-8be4-65195826cdff is eating. Its power became 56.
-# Herbivorous 75ebe620-5598-4cf4-a2c3-124546249918 is eating. Its power became 64.
-# >>>>>The Predator 4790bb9a-054a-419c-a30a-2e40aef6d59e is hunting the Prey cf52cd00-ed2c-467d-ab72-0ef43f4d3c3c...<<<<<
-# The Predator's 4790bb9a-054a-419c-a30a-2e40aef6d59e power became 29.
-# The Prey's Herbivorous cf52cd00-ed2c-467d-ab72-0ef43f4d3c3c power became 20.
-# Herbivorous 96b8486c-e1bc-4de9-8196-ff7401e0db6e is eating. Its power became 79.
-# >>>>>The Predator 50038426-1d14-4fde-a0e3-a4528f29f679 is hunting the Prey cf52cd00-ed2c-467d-ab72-0ef43f4d3c3c...<<<<<
-# The Predator's 50038426-1d14-4fde-a0e3-a4528f29f679 power became 32.
-# The Prey's Herbivorous cf52cd00-ed2c-467d-ab72-0ef43f4d3c3c power became 11.
-# Herbivorous 1094c043-fc6a-457b-b54d-ce96f63ab824 is eating. Its power became 26.
-# >>>>>The Predator 1f979e15-5310-4e9c-8e74-8ab98cee009a is hunting the Prey 4790bb9a-054a-419c-a30a-2e40aef6d59e...<<<<<
-# The Predator's 1f979e15-5310-4e9c-8e74-8ab98cee009a power became 35.
-# The Prey's Predator 4790bb9a-054a-419c-a30a-2e40aef6d59e power became 16.
-# Herbivorous c2ec45e3-0da2-48b7-a20c-9942848ccefa is eating. Its power became 75.
-# Herbivorous cf52cd00-ed2c-467d-ab72-0ef43f4d3c3c is eating. Its power became 26.
-# Herbivorous 073225a8-c7c5-40e7-8be4-65195826cdff is eating. Its power became 56.
-# Herbivorous 75ebe620-5598-4cf4-a2c3-124546249918 is eating. Its power became 64.
-# >>>>>The Predator 4790bb9a-054a-419c-a30a-2e40aef6d59e is hunting...<<<<<
-# The Predator 4790bb9a-054a-419c-a30a-2e40aef6d59e was left without a dinner and its power is 8!
-# Herbivorous 96b8486c-e1bc-4de9-8196-ff7401e0db6e is eating. Its power became 79.
-# >>>>>The Predator 50038426-1d14-4fde-a0e3-a4528f29f679 is hunting the Prey 1094c043-fc6a-457b-b54d-ce96f63ab824...<<<<<
-# The Predator's 50038426-1d14-4fde-a0e3-a4528f29f679 power became 18.
-# The Prey's Herbivorous 1094c043-fc6a-457b-b54d-ce96f63ab824 power became 18.
-# Herbivorous 1094c043-fc6a-457b-b54d-ce96f63ab824 is eating. Its power became 26.
-# >>>>>The Predator 1f979e15-5310-4e9c-8e74-8ab98cee009a is hunting the Prey cf52cd00-ed2c-467d-ab72-0ef43f4d3c3c...<<<<<
-# The Predator's 1f979e15-5310-4e9c-8e74-8ab98cee009a power became 9.
-# The Prey's Herbivorous cf52cd00-ed2c-467d-ab72-0ef43f4d3c3c power became 17.
-# Herbivorous c2ec45e3-0da2-48b7-a20c-9942848ccefa is eating. Its power became 75.
-# Herbivorous cf52cd00-ed2c-467d-ab72-0ef43f4d3c3c is eating. Its power became 29.
-# Herbivorous 073225a8-c7c5-40e7-8be4-65195826cdff is eating. Its power became 56.
-# Herbivorous 75ebe620-5598-4cf4-a2c3-124546249918 is eating. Its power became 64.
-# >>>>>The Predator 4790bb9a-054a-419c-a30a-2e40aef6d59e is hunting the Prey 50038426-1d14-4fde-a0e3-a4528f29f679...<<<<<
-# The Predator 4790bb9a-054a-419c-a30a-2e40aef6d59e loses the fight and dies.
-# =====Removing dead animal 4790bb9a-054a-419c-a30a-2e40aef6d59e...=====
-# The Prey's Predator 50038426-1d14-4fde-a0e3-a4528f29f679 power became 4.
-# Herbivorous 96b8486c-e1bc-4de9-8196-ff7401e0db6e is eating. Its power became 79.
-# >>>>>The Predator 50038426-1d14-4fde-a0e3-a4528f29f679 is hunting the Prey c2ec45e3-0da2-48b7-a20c-9942848ccefa...<<<<<
-# The Predator 50038426-1d14-4fde-a0e3-a4528f29f679 loses the fight and dies.
-# =====Removing dead animal 50038426-1d14-4fde-a0e3-a4528f29f679...=====
-# The Prey's Herbivorous c2ec45e3-0da2-48b7-a20c-9942848ccefa power became 52.
-# Herbivorous 1094c043-fc6a-457b-b54d-ce96f63ab824 is eating. Its power became 26.
-# >>>>>The Predator 1f979e15-5310-4e9c-8e74-8ab98cee009a is hunting the Prey 75ebe620-5598-4cf4-a2c3-124546249918...<<<<<
-# The Predator 1f979e15-5310-4e9c-8e74-8ab98cee009a loses the fight and dies.
-# =====Removing dead animal 1f979e15-5310-4e9c-8e74-8ab98cee009a...=====
-# The Prey's Herbivorous 75ebe620-5598-4cf4-a2c3-124546249918 power became 45.
-# Herbivorous c2ec45e3-0da2-48b7-a20c-9942848ccefa is eating. Its power became 75.
-# Herbivorous cf52cd00-ed2c-467d-ab72-0ef43f4d3c3c is eating. Its power became 29.
-# Herbivorous 073225a8-c7c5-40e7-8be4-65195826cdff is eating. Its power became 56.
-# Herbivorous 75ebe620-5598-4cf4-a2c3-124546249918 is eating. Its power became 64.
-# These animals survived in the forest: [Herbivorous 75ebe620-5598-4cf4-a2c3-124546249918,
-#                                        Herbivorous 75ebe620-5598-4cf4-a2c3-124546249918,
-#                                        Herbivorous 75ebe620-5598-4cf4-a2c3-124546249918,
-#                                        Herbivorous 75ebe620-5598-4cf4-a2c3-124546249918,
-#                                        Herbivorous 75ebe620-5598-4cf4-a2c3-124546249918,
-#                                        Herbivorous 75ebe620-5598-4cf4-a2c3-124546249918]
+# =====Added new animal Predator 31fcfbb0-f6dc-45ce-a9eb-d412f9796c31, power:64, speed: 78=====
+# =====Added new animal Herbivorous 680b2e03-92e9-44e6-a910-a5e0d3f38057, power:82, speed: 37=====
+# =====Added new animal Predator d85f5a96-fd05-48e8-8b89-878f31fc51d2, power:40, speed: 82=====
+# =====Added new animal Predator 53bb97d5-88f6-4a31-9a41-e5b0210a8684, power:44, speed: 92=====
+# =====Added new animal Herbivorous b1ce4a61-97e6-4e4e-8d31-223a3382712c, power:40, speed: 87=====
+# =====Added new animal Predator 1165137a-ad03-486c-b67f-b2b00258d80a, power:51, speed: 81=====
+# =====Added new animal Herbivorous 2d49ab52-8ae3-4c0b-868a-89e3b3d11158, power:65, speed: 44=====
+# =====Added new animal Predator 537cd817-d8d4-4120-a8e4-28879adecc38, power:43, speed: 82=====
+# =====Added new animal Predator f7f2823b-a52d-4988-9f01-048fe0c85de0, power:27, speed: 62=====
+# =====Added new animal Predator ac3c217f-919a-4434-9be7-231c3b6a7477, power:80, speed: 80=====
+# >>>>>The Predator 31fcfbb0-f6dc-45ce-a9eb-d412f9796c31 is hunting the Prey 53bb97d5-88f6-4a31-9a41-e5b0210a8684...<<<<<
+# The Predator's 31fcfbb0-f6dc-45ce-a9eb-d412f9796c31 power became 45.
+# The Prey's Predator 53bb97d5-88f6-4a31-9a41-e5b0210a8684 power became 31.
+# Herbivorous 680b2e03-92e9-44e6-a910-a5e0d3f38057 is eating. Its power became 82.
+# >>>>>The Predator d85f5a96-fd05-48e8-8b89-878f31fc51d2 is hunting the Prey 680b2e03-92e9-44e6-a910-a5e0d3f38057...<<<<<
+# The Predator's d85f5a96-fd05-48e8-8b89-878f31fc51d2 power became 28.
+# The Prey's Herbivorous 680b2e03-92e9-44e6-a910-a5e0d3f38057 power became 57.
+# >>>>>The Predator 53bb97d5-88f6-4a31-9a41-e5b0210a8684 is hunting the Prey 680b2e03-92e9-44e6-a910-a5e0d3f38057...<<<<<
+# The Predator's 53bb97d5-88f6-4a31-9a41-e5b0210a8684 power became 18.
+# The Prey's Herbivorous 680b2e03-92e9-44e6-a910-a5e0d3f38057 power became 32.
+# Herbivorous b1ce4a61-97e6-4e4e-8d31-223a3382712c is eating. Its power became 40.
+# >>>>>The Predator 1165137a-ad03-486c-b67f-b2b00258d80a is hunting the Prey 31fcfbb0-f6dc-45ce-a9eb-d412f9796c31...<<<<<
+# The Predator 1165137a-ad03-486c-b67f-b2b00258d80a kills the Prey 31fcfbb0-f6dc-45ce-a9eb-d412f9796c31. The Predator's power is 51
+# =====Removing dead animal 31fcfbb0-f6dc-45ce-a9eb-d412f9796c31...=====
+# Herbivorous 2d49ab52-8ae3-4c0b-868a-89e3b3d11158 is eating. Its power became 65.
+# >>>>>The Predator 537cd817-d8d4-4120-a8e4-28879adecc38 is hunting the Prey 53bb97d5-88f6-4a31-9a41-e5b0210a8684...<<<<<
+# The Predator's 537cd817-d8d4-4120-a8e4-28879adecc38 power became 30.
+# The Prey's Predator 53bb97d5-88f6-4a31-9a41-e5b0210a8684 power became 5.
+# >>>>>The Predator f7f2823b-a52d-4988-9f01-048fe0c85de0 is hunting the Prey d85f5a96-fd05-48e8-8b89-878f31fc51d2...<<<<<
+# The Predator's f7f2823b-a52d-4988-9f01-048fe0c85de0 power became 19.
+# The Prey's Predator d85f5a96-fd05-48e8-8b89-878f31fc51d2 power became 16.
+# >>>>>The Predator ac3c217f-919a-4434-9be7-231c3b6a7477 is hunting the Prey 537cd817-d8d4-4120-a8e4-28879adecc38...<<<<<
+# The Predator's ac3c217f-919a-4434-9be7-231c3b6a7477 power became 56.
+# The Prey's Predator 537cd817-d8d4-4120-a8e4-28879adecc38 power became 17.
+# Herbivorous 680b2e03-92e9-44e6-a910-a5e0d3f38057 is eating. Its power became 73.
+# >>>>>The Predator d85f5a96-fd05-48e8-8b89-878f31fc51d2 is hunting the Prey 1165137a-ad03-486c-b67f-b2b00258d80a...<<<<<
+# The Predator's d85f5a96-fd05-48e8-8b89-878f31fc51d2 power became 4.
+# The Prey's Predator 1165137a-ad03-486c-b67f-b2b00258d80a power became 36.
+# >>>>>The Predator 53bb97d5-88f6-4a31-9a41-e5b0210a8684 is hunting the Prey f7f2823b-a52d-4988-9f01-048fe0c85de0...<<<<<
+# The Predator 53bb97d5-88f6-4a31-9a41-e5b0210a8684 loses the fight and dies.
+# =====Removing dead animal 53bb97d5-88f6-4a31-9a41-e5b0210a8684...=====
+# The Prey's Predator f7f2823b-a52d-4988-9f01-048fe0c85de0 power became 11.
+# Herbivorous b1ce4a61-97e6-4e4e-8d31-223a3382712c is eating. Its power became 40.
+# >>>>>The Predator 1165137a-ad03-486c-b67f-b2b00258d80a is hunting the Prey b1ce4a61-97e6-4e4e-8d31-223a3382712c...<<<<<
+# The Predator's 1165137a-ad03-486c-b67f-b2b00258d80a power became 21.
+# The Prey's Herbivorous b1ce4a61-97e6-4e4e-8d31-223a3382712c power became 28.
+# Herbivorous 2d49ab52-8ae3-4c0b-868a-89e3b3d11158 is eating. Its power became 65.
+# >>>>>The Predator 537cd817-d8d4-4120-a8e4-28879adecc38 is hunting the Prey f7f2823b-a52d-4988-9f01-048fe0c85de0...<<<<<
+# The Predator 537cd817-d8d4-4120-a8e4-28879adecc38 kills the Prey f7f2823b-a52d-4988-9f01-048fe0c85de0. The Predator's power is 38
+# =====Removing dead animal f7f2823b-a52d-4988-9f01-048fe0c85de0...=====
+# >>>>>The Predator f7f2823b-a52d-4988-9f01-048fe0c85de0 is hunting the Prey 680b2e03-92e9-44e6-a910-a5e0d3f38057...<<<<<
+# The Predator's f7f2823b-a52d-4988-9f01-048fe0c85de0 power became 3.
+# The Prey's Herbivorous 680b2e03-92e9-44e6-a910-a5e0d3f38057 power became 48.
+# >>>>>The Predator ac3c217f-919a-4434-9be7-231c3b6a7477 is hunting the Prey d85f5a96-fd05-48e8-8b89-878f31fc51d2...<<<<<
+# The Predator's ac3c217f-919a-4434-9be7-231c3b6a7477 power became 32.
+# =====Removing dead animal d85f5a96-fd05-48e8-8b89-878f31fc51d2...=====
+# Herbivorous 680b2e03-92e9-44e6-a910-a5e0d3f38057 is eating. Its power became 82.
+# Herbivorous b1ce4a61-97e6-4e4e-8d31-223a3382712c is eating. Its power became 40.
+# >>>>>The Predator 1165137a-ad03-486c-b67f-b2b00258d80a is hunting the Prey b1ce4a61-97e6-4e4e-8d31-223a3382712c...<<<<<
+# The Predator's 1165137a-ad03-486c-b67f-b2b00258d80a power became 6.
+# The Prey's Herbivorous b1ce4a61-97e6-4e4e-8d31-223a3382712c power became 28.
+# Herbivorous 2d49ab52-8ae3-4c0b-868a-89e3b3d11158 is eating. Its power became 65.
+# >>>>>The Predator 537cd817-d8d4-4120-a8e4-28879adecc38 is hunting the Prey 2d49ab52-8ae3-4c0b-868a-89e3b3d11158...<<<<<
+# The Predator's 537cd817-d8d4-4120-a8e4-28879adecc38 power became 25.
+# The Prey's Herbivorous 2d49ab52-8ae3-4c0b-868a-89e3b3d11158 power became 46.
+# >>>>>The Predator ac3c217f-919a-4434-9be7-231c3b6a7477 is hunting the Prey 2d49ab52-8ae3-4c0b-868a-89e3b3d11158...<<<<<
+# The Predator's ac3c217f-919a-4434-9be7-231c3b6a7477 power became 8.
+# The Prey's Herbivorous 2d49ab52-8ae3-4c0b-868a-89e3b3d11158 power became 26.
+# Herbivorous 680b2e03-92e9-44e6-a910-a5e0d3f38057 is eating. Its power became 82.
+# Herbivorous b1ce4a61-97e6-4e4e-8d31-223a3382712c is eating. Its power became 40.
+# >>>>>The Predator 1165137a-ad03-486c-b67f-b2b00258d80a is hunting the Prey 537cd817-d8d4-4120-a8e4-28879adecc38...<<<<<
+# The Predator 1165137a-ad03-486c-b67f-b2b00258d80a loses the fight and dies.
+# =====Removing dead animal 1165137a-ad03-486c-b67f-b2b00258d80a...=====
+# The Prey's Predator 537cd817-d8d4-4120-a8e4-28879adecc38 power became 12.
+# Herbivorous 2d49ab52-8ae3-4c0b-868a-89e3b3d11158 is eating. Its power became 58.
+# >>>>>The Predator 537cd817-d8d4-4120-a8e4-28879adecc38 is hunting the Prey 2d49ab52-8ae3-4c0b-868a-89e3b3d11158...<<<<<
+# The Predator 537cd817-d8d4-4120-a8e4-28879adecc38 loses the fight and dies.
+# =====Removing dead animal 537cd817-d8d4-4120-a8e4-28879adecc38...=====
+# The Prey's Herbivorous 2d49ab52-8ae3-4c0b-868a-89e3b3d11158 power became 38.
+# >>>>>The Predator ac3c217f-919a-4434-9be7-231c3b6a7477 is hunting...<<<<<
+# The Predator ac3c217f-919a-4434-9be7-231c3b6a7477 was left without a dinner and its power is 4!
+# Herbivorous 680b2e03-92e9-44e6-a910-a5e0d3f38057 is eating. Its power became 82.
+# Herbivorous b1ce4a61-97e6-4e4e-8d31-223a3382712c is eating. Its power became 40.
+# Herbivorous 2d49ab52-8ae3-4c0b-868a-89e3b3d11158 is eating. Its power became 65.
+# >>>>>The Predator ac3c217f-919a-4434-9be7-231c3b6a7477 is hunting the Prey 680b2e03-92e9-44e6-a910-a5e0d3f38057...<<<<<
+# The Predator ac3c217f-919a-4434-9be7-231c3b6a7477 loses the fight and dies.
+# =====Removing dead animal ac3c217f-919a-4434-9be7-231c3b6a7477...=====
+# The Prey's Herbivorous 680b2e03-92e9-44e6-a910-a5e0d3f38057 power became 57.
+# These animals survived in the forest: [Herbivorous ac3c217f-919a-4434-9be7-231c3b6a7477, Herbivorous ac3c217f-919a-4434-9be7-231c3b6a7477, Herbivorous ac3c217f-919a-4434-9be7-231c3b6a7477]
 #
 # Process finished with exit code 0
