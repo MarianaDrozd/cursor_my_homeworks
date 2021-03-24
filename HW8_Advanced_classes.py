625b6198f3d00b15af4f0166d69ac3c238291243
MarianaDrozd <mariasyad@gmail.com>
2021-03-24 21:28:50 +0200

    Refactoring H8

--- HW8_Advanced_classes.py
+++ HW8_Advanced_classes.py
@@ -23,33 +23,33 @@ class Predator(Animal):
 
     def eat(self, forest: Forest):
         prey = random.choice(list(forest.animals.values()))
-
-        if self.id == prey.id:
-            print(f">>>>>The Predator {animal.id} is hunting...<<<<<")
-            self.current_power = round(self.current_power * 0.5)
-            print(f"The Predator {self.id} was left without a dinner and its power is {self.current_power}!")
-            if self.current_power == 0:
-                print(f"The Predator {animal.id} is dead by starving!")
-                forest.remove_animal(self)
-        else:
-            print(f">>>>>The Predator {animal.id} is hunting the Prey {prey.id}...<<<<<")
-            if self.speed > prey.speed and self.current_power > prey.current_power:
-                self.current_power = min(round(self.current_power + (self.max_power * 0.5)), self.max_power)
-                print(f"The Predator {animal.id} kills the Prey {prey.id}.",
-                      f"The Predator's power is {animal.current_power}")
-                prey.current_power = 0
-            else:
-                self.current_power = max(round(self.current_power - self.max_power * 0.3), 0)
-                if self.current_power <= 0:
-                    print(f"The Predator {animal.id} loses the fight and dies.")
-                else:
-                    print(f"The Predator's {animal.id} power became {animal.current_power}.")
-                prey.current_power = max(round(prey.current_power - prey.max_power * 0.3), 0)
-                print(f"The Prey's {prey.__class__.__name__} {prey.id} power became {prey.current_power}.")
         if self.current_power == 0:
-            forest.remove_animal(self)
-        if prey.current_power == 0:
-            forest.remove_animal(prey)
+            return
+        elif prey.current_power == 0:
+            return
+        else:
+            if self.id == prey.id:
+                print(f">>>>>The Predator {animal.id} is hunting...<<<<<")
+                self.current_power = round(self.current_power * 0.5)
+                print(f"The Predator {self.id} was left without a dinner and its power is {self.current_power}!")
+                if self.current_power == 0:
+                    print(f"The Predator {animal.id} is dead by starving!")
+
+            else:
+                print(f">>>>>The Predator {animal.id} is hunting the Prey {prey.id}...<<<<<")
+                if self.speed > prey.speed and self.current_power > prey.current_power:
+                    self.current_power = min(round(self.current_power + (self.max_power * 0.5)), self.max_power)
+                    print(f"The Predator {animal.id} kills the Prey {prey.id}.",
+                          f"The Predator's power is {animal.current_power}")
+                    prey.current_power = 0
+                else:
+                    self.current_power = max(round(self.current_power - self.max_power * 0.3), 0)
+                    if self.current_power <= 0:
+                        print(f"The Predator {animal.id} loses the fight and dies.")
+                    else:
+                        print(f"The Predator's {animal.id} power became {animal.current_power}.")
+                    prey.current_power = max(round(prey.current_power - prey.max_power * 0.3), 0)
+                    print(f"The Prey's {prey.__class__.__name__} {prey.id} power became {prey.current_power}.")
 
     def __repr__(self):
         return f"{__class__.__name__} {animal.id}"
@@ -58,9 +58,6 @@ class Predator(Animal):
 class Herbivorous(Animal):
 
     def eat(self, forest: Forest):
-        if self.current_power == 0:
-            forest.remove_animal(self.id)
-            return
         self.current_power = min(round(self.current_power + (self.max_power * 0.5)), self.max_power)
         print(f"{__class__.__name__} {animal.id} is eating. Its power became {self.current_power}.")
 
