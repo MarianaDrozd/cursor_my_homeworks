8d54d0ee744d8facf37ff980dc2995589087aef6
mariana <mariasyad@gmail.com>
2021-04-15 22:17:29 +0300

    Adding tests in wash and vacuum_cleaner

--- Test_HW10_Robot_Cleaner.py
+++ Test_HW10_Robot_Cleaner.py
@@ -4,18 +4,17 @@ import HW10_RobotCleaner
 
 class TestRobotCleaner(unittest.TestCase):
     def setUp(self):
-        self.vasya = HW10_RobotCleaner.RobotCleaner(-5, 500, 600)
+        self.vasya = HW10_RobotCleaner.RobotCleaner(-5, 500, 500)
         self.petya = HW10_RobotCleaner.RobotCleaner(0, 0, 0)
         self.zhorick = HW10_RobotCleaner.RobotCleaner("50", -350, 300)
         self.kuzia = HW10_RobotCleaner.RobotCleaner(60, 0, -60)
+        self.robots = [self.vasya, self.petya, self.zhorick, self.kuzia]
+
+    def test_init(self):
         with self.assertRaises(ValueError):
             self.r_test1 = HW10_RobotCleaner.RobotCleaner("60", "p", [300])
         with self.assertRaises(TypeError):
             self.r_test2 = HW10_RobotCleaner.RobotCleaner(None, {450}, "0")
-
-        self.robots = [self.vasya, self.petya, self.zhorick, self.kuzia]
-
-    def test_init(self):
         for r in self.robots:
             self.assertIsInstance(r.charge_battery, float, "Value is not a number!")
             self.assertIsInstance(r.trash_can, float, "Value is not a number!")
@@ -25,6 +24,8 @@ class TestRobotCleaner(unittest.TestCase):
         for r in self.robots:
             self.assertGreaterEqual(r.water_quantity, 0, "Negative value!")
             self.assertLessEqual(r.water_quantity, 1000, "More than 500!")
+            with self.assertRaises(HW10_RobotCleaner.NoWater):
+                HW10_RobotCleaner.RobotCleaner(90, 0, 0).wash()
 
     def test_vacuum_cleaner(self):
         for r in self.robots:
@@ -32,21 +33,21 @@ class TestRobotCleaner(unittest.TestCase):
             self.assertLessEqual(r.charge_battery, 100, "More than 100!")
             self.assertGreaterEqual(r.trash_can, 0, "Negative value!")
             self.assertLessEqual(r.trash_can, 500, "More than 500!")
+            with self.assertRaises(HW10_RobotCleaner.LowBattery):
+                HW10_RobotCleaner.RobotCleaner(9, 250, 500).vacuum_cleaner()
+            with self.assertRaises(HW10_RobotCleaner.NoCharge):
+                HW10_RobotCleaner.RobotCleaner(0, 100, 200).vacuum_cleaner()
+            with self.assertRaises(HW10_RobotCleaner.FullTrash):
+                HW10_RobotCleaner.RobotCleaner(50, 501, 400).vacuum_cleaner()
 
     def test_move(self):
-        i = 1
         for r in self.robots:
-            print(f"=========Test move {i} {r.__class__.__name__}=========")
-            i += 1
             with self.assertRaises(SystemExit):
                 try:
                     r.move()
                 except HW10_RobotCleaner.NoCharge:
                     break
-        return
-
-    def tearDown(self):
-        pass
+            return
 
 
 if __name__ == "__main__":
